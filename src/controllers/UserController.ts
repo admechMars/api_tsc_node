2ce68c83a7d7271cc7fd83c7b75ef4b95fa3d103
admechMars <savioramonnogueira@gmail.com>
2024-04-14 23:36:52 -0300

    implememtação e refatoração de Controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,21 +1,19 @@
 import { Request, Response } from "express";
-import { UserServices } from "../services/UserServices";
+import { UserService } from "../services/UserServices";
 import { error } from "console";
 
 export class UserController {
 
-    userService: UserServices
+  userService: UserService
 
-    constructor(
-        userService = new UserServices()
-    ){
-        this.userService = userService
-    }
+  constructor(
+    userService = new UserService()
+  ) {
+    this.userService = userService
+  }
 
   createUsers = (request: Request, response: Response) => {
     const user = request.body;
-
-
 
     if (!user.name || !user.email || !user.password) {
       return response.status(400).json({ message: "Bad request!", error });
@@ -25,10 +23,42 @@ export class UserController {
     return response.status(201).json({ message: "Post created!" });
   };
 
-  getUser = (request: Request, response: Response) => {
-    const users = this.userService.getUser();
-    return response.status(200).json(users);
-  };
+  getUser = async (request: Request, response: Response) => {
+    const id = request.query.user_id
 
-  deleteUsers = (request: Request, response: Response) => {};
+    if(typeof(id) === 'string') {
+        const user = await this.userService.getUser(id)
+
+        if(user !== null){
+            return response.status(200).json(user)
+        } 
+
+        return response.status(200).json({message: 'Nenhum Usuário encontrado!'})
+    }
+
+    return response.status(400).json({message: 'Digite um id válido'})
+
+}
+
+  getAllUsers = async (request: Request, response: Response) => {
+    const users = await this.userService.getAllUsers()
+    return response.status(200).json(users)
+  }
+
+  deleteUser = async (request: Request, response: Response) => {
+    const id = request.query.user_id
+
+    if(typeof(id) === 'string') {
+        const status = await this.userService.deleteUser(id)  
+
+        if (status){
+            return response.status(200).json({message: `Usuário ${id} Deletado com sucesso!`})
+        } 
+        
+        return response.status(400).json({message: `Problemas ao Deletar usuário ${id}!`})
+
+    } else {
+        return response.status(400).json({message: 'Digite um id válido'})
+    }
+  };
 }
