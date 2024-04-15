a84cb298c4a549ded327992f54abd80198dc4ef5
admechMars <savioramonnogueira@gmail.com>
2024-04-14 23:37:16 -0300

    implementação e refatoração de Repository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,26 +1,35 @@
 import { EntityManager } from "typeorm";
-import { AppDataSource } from "../database";
 import { User } from "../entities/User";
 
+export class UserRepository {
+  private manager: EntityManager;
 
-export class UserRepository{
-    private manager : EntityManager
+  constructor(manager: EntityManager) {
+    this.manager = manager;
+  }
 
-    constructor(
-        manager =  AppDataSource.manager
-    ){
-        this.manager = manager;
+  createUser = async (user: User): Promise<User> => {
+    return this.manager.save(user);
+  };
+
+  getAllUsers = async (): Promise<User[]> => {
+    return await this.manager.find(User);
+  };
+
+  getUser = async (id_user: string): Promise<User | null> => {
+    return await this.manager.findOne(User, {
+      where: {
+        id_user: id_user,
+      },
+    });
+  };
+
+  deleteUser = async (id_user: string): Promise<boolean> => {
+    const user = await this.getUser(id_user);
+    if (user) {
+      this.manager.remove(user);
+      return true;
     }
-
-    createUser = async (user : User) => {
-        return this.manager.save(user)
-    }
-
-    getuser = async (IdUser : string): Promise<User | null > =>{
-        return this.manager.findOne(User, {
-            where: {
-                id_user: IdUser
-            }
-        })
-    }
+    return false;
+  };
 }
