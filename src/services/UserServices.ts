217b9ae40d60182ba061a5410891dff9e91a4183
admechMars <savioramonnogueira@gmail.com>
2024-04-14 23:37:38 -0300

    implementação e refatoração de Services

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -2,21 +2,28 @@ import { AppDataSource } from "../database";
 import { User } from "../entities/User";
 import { UserRepository } from "../repositories/UserRepository"
 
-export class UserServices{
-    private userRepository : UserRepository;
+export class UserService {
+    private userRepository: UserRepository;
 
-    constructor(
-        userRepository = new UserRepository(AppDataSource.manager),
-    ){
-        this.userRepository = userRepository
+    constructor(userRepository = new UserRepository(AppDataSource.manager)){
+        this.userRepository = userRepository;
     }
-
-    createUser = async (name: string, email:string, password: string) : Promise<User> =>{
+    
+    createUser = (name: string, email: string, password: string) => {
         const user = new User(name, email, password)
         return this.userRepository.createUser(user)
     }
 
-    getUser = () =>{
+    getAllUsers = async (): Promise<User[]> => {
+        return await this.userRepository.getAllUsers()
     }
-    
+
+    getUser = async (id: string):Promise<User | null> => {
+        return await this.userRepository.getUser(id)
+    }
+
+    deleteUser = async (id: string): Promise<boolean> => {
+        return await this.userRepository.deleteUser(id)
+    }
+
 }
